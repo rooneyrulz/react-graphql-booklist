dcbd91db16392e69a6cc6fbfef47a82925514938
ruzny <ruzny@ruzny.com>
2020-10-19 19:14:58 +0530

    modularized mongo connection to seperate file

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+require('colors');
+
+module.exports = async (server) => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    if (conn) {
+      console.log(`mongodb connection successful!!`.cyan);
+      server.listen(process.env.PORT | 5000, () =>
+        console.log(
+          `server running on ${process.env.NODE_ENV} mode on port ${process.env
+            .PORT | 5000}!!`.black.bgWhite
+        )
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
--- server.js
+++ server.js
@@ -1,40 +1,21 @@
 const express = require('express');
 const { createServer } = require('http');
 const loggerHTTP = require('morgan');
-const mongoose = require('mongoose');
 const dotENV = require('dotenv');
-require('colors');
+
+// Mongo Connection
+const db = require('./config/db');
 
 const app = express();
 const server = createServer(app);
 
 dotENV.config({ path: './config/config.env' });
 
+// Establish Mongo Connection
+db(server);
+
 if (process.env.NODE_ENV === 'development') app.use(loggerHTTP('dev'));
 
 app.use('', (req, res, next) => res.status(200).send('Hello World!'));
 
-async function init() {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-    if (conn) {
-      console.log(`mongodb connection successful!!`.cyan);
-      server.listen(process.env.PORT | 5000, () =>
-        console.log(
-          `server running on ${process.env.NODE_ENV} mode on port ${process.env
-            .PORT | 5000}!!`.black.bgWhite
-        )
-      );
-    }
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
-
-init();
+module.exports = app;
