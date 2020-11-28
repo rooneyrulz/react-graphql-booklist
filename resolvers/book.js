7e55a4bc2b77776ffb907aff4cd16db192ce25fe
ruzny <ruzny@ruzny.com>
2020-11-28 18:51:49 +0530

    added func for update book & remove book

--- resolvers/book.js
+++ resolvers/book.js
@@ -49,4 +49,59 @@ module.exports = {
       throw error;
     }
   },
+
+  // Update Book
+  updateBook: async (args, req) => {
+    if (!req.isAuth) return new Error('Authorization failed!');
+
+    const {
+      id,
+      bookInput: { name, description },
+    } = args;
+
+    try {
+      const user = await User.findById(req.user._id).lean();
+      if (!user) return new Error('Auth user not found, Access denied!');
+
+      const book = await Book.findById(id).lean();
+      if (!book) return new Error('Book not found!');
+
+      if (book.author.toString() !== user._id.toString())
+        return new Error('Access denied, Forbidden');
+
+      const updatedBook = await Book.findByIdAndUpdate(
+        id,
+        { name, description, author: req.user._id },
+        { new: true }
+      ).lean();
+      return {
+        ...updatedBook._doc,
+        author: await getUserById(updatedBook.author),
+      };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  // Remove Book
+  removeBook: async (args, req) => {
+    if (!req.isAuth) return new Error('Authorization failed!');
+
+    const { id } = args;
+
+    try {
+      const user = await User.findById(req.user.id).lean();
+      if (!user) return new Error('Access denied, Auth user not found!');
+
+      const book = await Book.findById(id).lean();
+      if (!book) return new Error('Book not found!');
+
+      if (book.author.toString() !== user._id.toString())
+        return new Error('Access denied, Forbidden');
+      await book.remove();
+      return `Book with id ${book._id} has been successfully removed!`;
+    } catch (error) {
+      throw error;
+    }
+  },
 };
