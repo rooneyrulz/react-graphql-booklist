a7a1082b9dfd0d9c9d998a32196a92fa20169413
ruzny <ruzny@ruzny.com>
2020-12-10 15:35:05 +0530

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,38 @@
-const App = () => <h1>Hello</h1>;
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// Layouts
+import AppHeader from './layouts/AppHeader';
+import AppFooter from './layouts/AppFooter';
+
+// Pages
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Books from './pages/Books';
+import MyBooks from './pages/MyBooks';
+
+// Auth Pages
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+
+const App = () => {
+  return (
+    <>
+      <AppHeader />
+      <main>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/books' component={Books} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/my-books' component={MyBooks} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
+      </main>
+      <AppFooter />
+    </>
+  );
+};
 
 export default App;
