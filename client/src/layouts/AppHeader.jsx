348f6ce6e9c8f438350f0b003a2c66a4f4dfb23e
ruzny <ruzny@ruzny.com>
2020-12-13 14:25:38 +0530

    header links | authLinks | guestLinks

--- client/src/layouts/AppHeader.jsx
+++ client/src/layouts/AppHeader.jsx
@@ -3,6 +3,47 @@ import { NavLink, Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const AppHeader = () => {
+  const [isAuth, setIsAuth] = React.useState(null);
+
+  const onLogIn = (e) => setIsAuth((prev) => true);
+  const onLogOut = (e) => setIsAuth((prev) => false);
+
+  const authLinks = (
+    <>
+      <NavLink exact to='/dashboard' className='nav-link'>
+        Dashboard
+      </NavLink>
+      <NavLink exact to='/my-books' className='nav-link'>
+        MyBooks
+      </NavLink>
+      <Link to='#' className='nav-link' onClick={(e) => onLogOut(e)}>
+        Logout
+      </Link>
+    </>
+  );
+
+  const guestLinks = (
+    <>
+      <NavLink exact to='/' className='nav-link'>
+        Home
+      </NavLink>
+      <NavLink exact to='/books' className='nav-link'>
+        Books
+      </NavLink>
+      <NavLink exact to='/register' className='nav-link'>
+        Register
+      </NavLink>
+      <NavLink
+        exact
+        to='/login'
+        className='nav-link'
+        onClick={(e) => onLogIn(e)}
+      >
+        Login
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar bg='light' expand='md'>
       <Container>
@@ -11,14 +52,7 @@ const AppHeader = () => {
         </Link>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ml-auto'>
-            <NavLink exact to='/register' className='nav-link'>
-              Register
-            </NavLink>
-            <NavLink exact to='/login' className='nav-link'>
-              Login
-            </NavLink>
-          </Nav>
+          <Nav className='ml-auto'>{isAuth ? authLinks : guestLinks}</Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
